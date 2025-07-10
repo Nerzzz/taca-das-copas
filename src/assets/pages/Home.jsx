e16e6df1c1dc56b4f6672bb17b8d414b2ca174e5
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2025-07-10 11:17:41 -0300

    Melhoroa no design do Pódio

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -8,23 +8,24 @@ function Home() {
         <main className='px-[10px] md:px-[40px] my-[40px]'>
             <section id='podio'>
                 <h1 className='text-[20pt] font-sora font-semibold'>Pódio</h1>
-                <div className='flex flex-row items-end justify-center gap-[20px] h-[200px]'>
+                <div className='flex flex-row items-end justify-center gap-[20px] mt-[50px]'>
                     <div className='flex flex-col items-center gap-[20px]'>
-                        <h1>3º DS</h1>
-                        <div className='bg-linear-to-b from-orange-300 to-orange-400 p-[30px] flex flex-col justify-center'>
-                            <span className='text-white text-[24pt] font-sora font-extrabold'>3</span>
+                        <h1 className='text-[16pt] font-sora font-medium'>3º DS</h1>
+                        <div className='bg-linear-to-b from-orange-300 to-orange-400 p-[30px] flex flex-col justify-center w-fit text-center shadow'>
+                            <span className='text-white text-[24pt] font-sora font-extrabold'>3º</span>
                         </div>
                     </div>
                     <div className='flex flex-col items-center gap-[20px]'>
-                        <h1>3º DS</h1>
-                        <div className='bg-linear-to-b from-yellow-400 to-yellow-500 p-[30px] h-[100%] flex flex-col justify-center'>
-                            <span className='text-white text-[24pt] font-sora font-extrabold'>1</span>
+                        <h1 className='text-[16pt] font-sora font-medium'>3º DS</h1>
+                        <div className='bg-linear-to-b from-yellow-400 to-yellow-500 p-[30px] h-[200px] flex flex-col justify-between items-center w-[100px] text-center gap-[20px] shadow'>
+                            <i className='bi bi-award-fill text-white text-[24pt]' />
+                            <span className='text-white text-[24pt] font-sora font-extrabold'>1º</span>
                         </div>
                     </div>
                     <div className='flex flex-col items-center gap-[20px]'>
-                        <h1>3º DS</h1>
-                        <div className='bg-linear-to-b from-gray-300 to-gray-400 p-[30px] h-[20%] flex flex-col justify-center'>
-                            <span className='text-white text-[24pt] font-sora font-extrabold'>2</span>
+                        <h1 className='text-[16pt] font-sora font-medium'>3º DS</h1>
+                        <div className='bg-linear-to-b from-gray-300 to-gray-400 p-[30px] pt-[70px] flex flex-col justify-center w-fit text-center shadow'>
+                            <span className='text-white text-[24pt] font-sora font-extrabold'>2º</span>
                         </div>
                     </div>
                 </div>
