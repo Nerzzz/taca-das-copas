f030041ad8dd6388331b6a2f87f1b361fb4066c6
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2025-07-12 13:33:20 -0300

    Tela de Login Finalizada

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,5 +1,5 @@
 import react, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../firebase.config'
 
@@ -22,16 +22,17 @@ function Login() {
     }
 
     return (
-        <section className='flex justify-center items-center h-[80vh]'>
-            <div className='shadow'>
-                <h1>Taça das Copas</h1>
-                <form action="" onSubmit={handleLogin}>
+        <section className='flex justify-center items-center h-[100vh]'>
+            <div className='shadow px-[25px] py-[40px] rounded-[10px]'>
+                <h1 className='text-[20pt] font-sora font-semibold'>Painel <span className='text_grad'>Administrativo</span></h1>
+                <form action="" onSubmit={handleLogin} className='flex flex-col gap-[20px] mt-[50px]'>
 
                     <input
                         type="email"
                         placeholder="Email"
                         value={email}
                         onChange={e => setEmail(e.target.value)}
+                        className='py-[5px] px-[10px] bg-gray-100 font-poppins text-[12pt] outline-none focus:border-orange-30 border-[1px] border-gray-300 rounded-[5px]'
                     />
 
                     <input
@@ -39,10 +40,13 @@ function Login() {
                         placeholder="Senha"
                         value={senha}
                         onChange={e => setSenha(e.target.value)}
+                        className='py-[5px] px-[10px] bg-gray-100 font-poppins text-[12pt] outline-none focus:border-orange-30 border-[1px] border-gray-300 rounded-[5px]'
                     />
 
-                    <button type="submit">Entrar</button>
-
+                    <nav className='flex justify-between mt-[30px] items-center'>
+                        <input type="submit" className='px-[20px] py-[5px] w-fit bg-linear-to-l from-orange-10 to-orange-20 text-[14pt] font-medium text-white cursor-pointer rounded-[5px] hover:scale-[1.075] hover:shadow-xl duration-[0.5s]' value={"Entrar"} />
+                        <Link to={"/"} className='text-[13pt] text-blue-500 underline'>Voltar</Link>
+                    </nav>
                 </form>
             </div>
         </section>
